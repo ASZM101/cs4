61c43ba35aa7c568dfaca23ac26062331ae483b4
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-09-05 05:45:47 +0000

    Input for record fucn (tracker)

--- 02-health-tracker/tracker.cpp
+++ 02-health-tracker/tracker.cpp
@@ -1,13 +1,15 @@
 // standard library headers
 #include <format>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
-// func declarations (both actual def AND prototype MUST match)
-void handleMenu(std::string input);
-void record();
-void evaluate();
-void save();
+// func declarations (both actual def AND prototype MUST match; pass patients vector to all funcs)
+void handleMenu(std::string input, std::vector<DailyVitals> &patients);
+void record(std::vector<DailyVitals> &patients);
+void evaluate(std::vector<DailyVitals> &patients);
+void save(std::vector<DailyVitals> &patients);
 
 // define class to store attributes and methods dealing with daily vitals
 class DailyVitals {
@@ -61,6 +63,7 @@ class DailyVitals {
 
 // display menu options [=> used for single-line outputs, (!) used for errors]
 int main() {
+    std::vector<DailyVitals> patients;
     std::string input = "";
     std::cout << "Welcome to the Health Tracker!\n";
     while (input != "4") {
@@ -72,20 +75,20 @@ int main() {
 Enter selected option (ie. 1, 2, 3, 4): )";
         std::cout << std::format("\n{}", menu);
         std::cin >> input;
-        handleMenu(input);
+        handleMenu(input, patients);
     }
     std::cout << "=> Thank you for using the Health Tracker!\n";
     return 0;
 }
 
 // execute selected menu option based on input
-void handleMenu(std::string input) {
+void handleMenu(std::string input, std::vector<DailyVitals> &patients) {
     if (input == "1") {
-        record();
+        record(patients);
     } else if (input == "2") {
-        evaluate();
+        evaluate(patients);
     } else if (input == "3") {
-        save();
+        save(patients);
     } else if (input == "4") {
         return;
     } else {
@@ -94,23 +97,86 @@ void handleMenu(std::string input) {
 }
 
 // [1] record given health vitals (ask for input, store in vars)
-void record() {
+void record(std::vector<DailyVitals> &patients) {
     std::string input = "";
     std::string username = "";
     int heartRate = 0;
     int steps = 0;
     bool medTaken = false;
     std::string errorInt = "(!) Please only type a positive integer."; // error msg displayed after invalid input for int
-    std::string errorBool = "(!) Please only type either 1 (medication taken) or 0 (medication not taken)."; // error msg displayed after invalid input for bool
+    std::string errorBool = "(!) Please only type either 1 (medication taken or not applicable) or 0 (medication not taken)."; // error msg displayed after invalid input for bool
     bool error = false;
+    std::cout << "Enter patient username (create username for new users, type existing username for returning users): ";
+    std::cin >> username;
+    while (input.length() < 1 || error) { // get input for resting heart rate
+        std::cout << "Enter resting heart rate (BPM): ";
+        std::cin >> input;
+        std::stringstream convert(input);
+        if (convert >> heartRate && heartRate > 0) { // try to convert str from stream to int, ensure input is positive
+            error = false;
+        } else {
+            error = true;
+            std::cout << std::format("{}\n", errorInt);
+        }
+    }
+    input = ""; // reset input for next while loop
+    while (input.length() < 1 || error) { // get input for step count
+        std::cout << "Enter step count: ";
+        std::cin >> input;
+        std::stringstream convert(input);
+        if (convert >> steps && steps > 0) { // try to convert str from stream to int, ensure input is positive
+            error = false;
+        } else {
+            error = true;
+            std::cout << std::format("{}\n", errorInt);
+        }
+    }
+    input = ""; // reset input for next while loop
+    while (input.length() < 1 || error) { // get input for medication status
+        std::cout << "Enter medication status (1 = taken or not applicable, 0 = missed): ";
+        std::cin >> input;
+        if (input == "1" || input == "0") { // ensure input is either 1 or 0
+            error = false;
+            medTaken = input == "1" ? true : false;
+        } else {
+            error = true;
+            std::cout << std::format("{}\n", errorBool);
+        }
+    }
+    std::cout << std::format("=> Username: {}\n", username);
+    std::cout << std::format("=> Resting heart rate: {}\n", heartRate);
+    std::cout << std::format("=> Step count: {}\n", steps);
+    std::cout << std::format("=> Medication status: {}\n", medTaken);
 }
 
 // [2] evaluate given health vitals (compare stored vars to pre-defined thresholds, output health scores / ratings)
-void evaluate() {
-    std::cout << "Evaluate mode selected.\n";
+void evaluate(std::vector<DailyVitals> &patients) {
+    std::string input = "";
+    std::string username = "";
+    int heartRate = 0;
+    int steps = 0;
+    bool medTaken = false;
+    std::string errorInt = "(!) Please only type a positive integer."; // error msg displayed after invalid input for int
+    std::string errorBool = "(!) Please only type either 1 (medication taken or not applicable) or 0 (medication not taken)."; // error msg displayed after invalid input for bool
+    bool error = false;
+    std::cout << "Enter patient username (only for returning users): ";
+    std::cin >> username;
+    // username not found: ask user if they would like to create account and record today's vitals, call record func
+    // vitals not recorded: ask user if they would like to record today's vitals, call record func
 }
 
 // [3] save evaluation summary in log (append formatted values to log file, create log file if doesn't exist yet)
-void save() {
-    std::cout << "Save mode selected.\n";
+void save(std::vector<DailyVitals> &patients) {
+    std::string input = "";
+    std::string username = "";
+    int heartRate = 0;
+    int steps = 0;
+    bool medTaken = false;
+    std::string errorInt = "(!) Please only type a positive integer."; // error msg displayed after invalid input for int
+    std::string errorBool = "(!) Please only type either 1 (medication taken or not applicable) or 0 (medication not taken)."; // error msg displayed after invalid input for bool
+    bool error = false;
+    std::cout << "Enter patient username (only for returning users): ";
+    std::cin >> username;
+    // username not found: ask user if they would like to create account and record today's vitals, call record func
+    // vitals not recorded: ask user if they would like to record today's vitals, call record func
 }
